8e5eaee6aacf381fb6f9f2fd19d354e8ca9bfe93
Nysalie <67501169+Nysalie@users.noreply.github.com>
2020-06-28 21:28:26 +0100

    Documentation for IDE Support

--- Calendar/Runtime/Commands/AddTime.cs
+++ Calendar/Runtime/Commands/AddTime.cs
@@ -3,11 +3,14 @@ using Naninovel.Commands;
 using System.Threading;
 using UniRx.Async;
 
+[Documentation("Adds time to game's calendar in the form of days and/or hours.")]
 namespace NaninovelCalendar
 {
     public class AddTime : Command
     {
+        [Documentation("Hour count to add.")]
         public IntegerParameter Hours = 0;
+        [Documentation("Day count to add.")]
         public IntegerParameter Days = 0;
 
         public override async UniTask ExecuteAsync (CancellationToken cancellationToken = default)
